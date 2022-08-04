61a5a30c6cf82d4893c0c76e668ffd18b4c48a22
oktavianoandy <oktaviano.andy.suryadi@gmail.com>
2022-08-04 09:31:57 +0700

    login get data

--- server.py
+++ server.py
@@ -21,9 +21,6 @@ def login():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # username = "admin"
-    # password = "21232f297a57a5a743894a0e4a801fc3"
-
     data = get_user_by_uname_pass(username, password)
 
     return jsonify(data)
