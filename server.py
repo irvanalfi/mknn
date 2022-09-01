8172d66a28d411007dec1cc13573dd61b83616ec
irvanalfi <44670772+irvanalfi@users.noreply.github.com>
2022-09-01 18:32:55 +0700

    db baru

--- server.py
+++ server.py
@@ -216,62 +216,71 @@ def import_testing():
 
 @app.route('/halaman-pengujian', methods=['GET', 'POST'])
 def halaman_proses_pengujian():
-    data_testing = db_get_all_testing()
-    data_training = db_get_all_training()
+    data_testing = db_get_seratus_testing()
+    data_training = db_get_seratus_training()
     data = {
         "data_testing": data_testing,
-        "data_training": data_training,
-        # "tf_idf" :
+        "data_training": data_training
     }
     return jsonify(data)
 
 
 @app.route('/pengujian', methods=['GET', 'POST'])
 def pengujian():
-    # proses tf-idf training dan testing
-    tfidf(get_hasil())
-    # read csv hasil tf-idf
+
     df = pd.read_csv('C:/Users/IRVAN/backendmknn/Upload/tfidf.csv', sep=';')
     # membagi hasil tf idf dari total data set ke 600 training, 400 testing
     split1 = (df.shape[0] * 6) / 10
-    # baca hasil split tf-idf data training dan testing
     df1 = pd.DataFrame(df.iloc[:int(split1)])
     df2 = pd.DataFrame(df.iloc[int(split1):])
     # proses jarak euclidean dan menyimpan data ke dalam file csv
-    # jarak_dtdt = jarakeuclideanDTDT(df1)
-    # jarak_dtds = jarakeuclideanDTDS(df1, df2, split1)
-    # read csv jarak_dtdt dan jarak_dtds
-    jarak_dtdt = pd.read_csv('C:/Users/IRVAN/backendmknn/Upload/euclideandtdt.csv', sep=',')
-    jarak_dtds = pd.read_csv('C:/Users/IRVAN/backendmknn/Upload/euclideandtds.csv', sep=',')
-    # mengurutkan data jarak_dtdt
-    small_dtdt = small(jarak_dtdt)
-    # jumlah k yang digunakan
-    k = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
-    for j in tqdm(k):
-        idx = 1
-        nama_polaritas = "polaritas_akhir_k" + str(j)
-        k_dtdt = k_euclidean(small_dtdt, j)
-        k_dtdt.to_csv('C:/Users/IRVAN/backendmknn/Upload/k11euclideandtdt.csv', index=False)
-        lokasi_dtdt = lokasi(jarak_dtdt, j)
-        lokasi_dtdt.to_csv('C:/Users/IRVAN/backendmknn/Upload/lokasiargsmalleuclideandtdt.csv', index=False)
-        label_train = get_label("training")
-        label_test = get_polaritas("polaritas_awal")
-        pelabelan_dtdt = pelabelan(lokasi_dtdt, label_train)
-        pelabelan_dtdt.to_csv('C:/Users/IRVAN/backendmknn/Upload/labelargsmalleuclideandtdt.csv', index=False)
-        validitas_dtdt = validitas(pelabelan_dtdt, label_train)
-        validitas_dtdt.to_csv('C:/Users/IRVAN/backendmknn/Upload/valargsmalleuclideandtdt.csv', index=False)
-        lbl = []
-        for i in tqdm(range(len(jarak_dtds.iloc[0])), leave=False):
-            rank, label = ranking(validitas_dtdt, jarak_dtds[str(i)], j)
-            lbl2 = []
-            for i in label:
-                lbl2.append(label_train[i])
-            labelterdekat = pd.DataFrame(lbl2)
-            labelterdekat.to_json('C:/xampp/htdocs/mknnfrontend/assets/json/labelterdekat.json')
-            labelterdekat.to_csv('C:/Users/IRVAN/backendmknn/Upload/labelterdekat.csv')
-            lbl.append(most_frequent(lbl2))
-            updateData(nama_polaritas, most_frequent(lbl2), idx)
-            idx += 1
+    jarakeuclideanDTDT(df1)
+    jarakeuclideanDTDS(df1, df2, split1)
+
+    # # proses tf-idf training dan testing
+    # tfidf(get_hasil())
+    # # read csv hasil tf-idf
+    # df = pd.read_csv('C:/Users/IRVAN/backendmknn/Upload/tfidf.csv', sep=';')
+    # # membagi hasil tf idf dari total data set ke 600 training, 400 testing
+    # split1 = (df.shape[0] * 6) / 10
+    # # baca hasil split tf-idf data training dan testing
+    # df1 = pd.DataFrame(df.iloc[:int(split1)])
+    # df2 = pd.DataFrame(df.iloc[int(split1):])
+    # # proses jarak euclidean dan menyimpan data ke dalam file csv
+    # # jarak_dtdt = jarakeuclideanDTDT(df1)
+    # # jarak_dtds = jarakeuclideanDTDS(df1, df2, split1)
+    # # read csv jarak_dtdt dan jarak_dtds
+    # jarak_dtdt = pd.read_csv('C:/Users/IRVAN/backendmknn/Upload/euclideandtdt.csv', sep=',')
+    # jarak_dtds = pd.read_csv('C:/Users/IRVAN/backendmknn/Upload/euclideandtds.csv', sep=',')
+    # # mengurutkan data jarak_dtdt
+    # small_dtdt = small(jarak_dtdt)
+    # # jumlah k yang digunakan
+    # k = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+    # for j in tqdm(k):
+    #     idx = 1
+    #     nama_polaritas = "polaritas_akhir_k" + str(j)
+    #     k_dtdt = k_euclidean(small_dtdt, j)
+    #     k_dtdt.to_csv('C:/Users/IRVAN/backendmknn/Upload/k11euclideandtdt.csv', index=False)
+    #     lokasi_dtdt = lokasi(jarak_dtdt, j)
+    #     lokasi_dtdt.to_csv('C:/Users/IRVAN/backendmknn/Upload/lokasiargsmalleuclideandtdt.csv', index=False)
+    #     label_train = get_label("training")
+    #     label_test = get_polaritas("polaritas_awal")
+    #     pelabelan_dtdt = pelabelan(lokasi_dtdt, label_train)
+    #     pelabelan_dtdt.to_csv('C:/Users/IRVAN/backendmknn/Upload/labelargsmalleuclideandtdt.csv', index=False)
+    #     validitas_dtdt = validitas(pelabelan_dtdt, label_train)
+    #     validitas_dtdt.to_csv('C:/Users/IRVAN/backendmknn/Upload/valargsmalleuclideandtdt.csv', index=False)
+    #     lbl = []
+    #     for i in tqdm(range(len(jarak_dtds.iloc[0])), leave=False):
+    #         rank, label = ranking(validitas_dtdt, jarak_dtds[str(i)], j)
+    #         lbl2 = []
+    #         for i in label:
+    #             lbl2.append(label_train[i])
+    #         labelterdekat = pd.DataFrame(lbl2)
+    #         labelterdekat.to_json('C:/xampp/htdocs/mknnfrontend/assets/json/labelterdekat.json')
+    #         labelterdekat.to_csv('C:/Users/IRVAN/backendmknn/Upload/labelterdekat.csv')
+    #         lbl.append(most_frequent(lbl2))
+    #         updateData(nama_polaritas, most_frequent(lbl2), idx)
+    #         idx += 1
     status = "success"
     return status
 
