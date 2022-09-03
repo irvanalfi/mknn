e289b506a2436cfbb99e7c2ff3fa05c77a0594d0
latif-abdul <1841720110@student.polinema.ac.id>
2022-09-03 11:15:20 +0700

    2

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from flask import Flask
-import socket
 import pandas
 from tqdm import tqdm
 from IPython.display import display
@@ -7,35 +5,9 @@ from IPython.display import display
 # from db import db_import_data_testing, db_import_data_training, get_hasil, get_label, updateData
 # from preprocess import *
 from process import *
-my_ip = socket.gethostbyname(socket.gethostname())
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def dashboar():
-    data = {
-        "datatesting": "200",
-        "datatraining": "800"
-    }
-    return data
-
-
-def most_frequent(List):
-    counter = 0
-    num = List[0]
-
-    for i in List:
-        curr_frequency = List.count(i)
-        if (curr_frequency > counter):
-            counter = curr_frequency
-            num = i
-
-    return num
 
 
 if __name__ == "__main__":
-    # app.run(host=my_ip)
-    # db_get_all_training()
     # db_import_data_training("Upload/training.csv")
     # db_import_data_testing("Upload/testing.csv")
     # pelabelanOtomatis()
@@ -54,7 +26,7 @@ if __name__ == "__main__":
     # jarak_dtds = pandas.read_csv('D:/github/mknn/Upload/euclideandtds.csv', sep=',')
     #
     # small_dtdt = small(jarak_dtdt)
-    #
+    # #jumlah k yang digunakan
     # k = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
     # idx = 1
     # for j in tqdm(k):
@@ -82,11 +54,14 @@ if __name__ == "__main__":
     #         lbl2 = []
     #         for i in label:
     #             lbl2.append(label_train[i])
+    #         labelterdekat = pandas.DataFrame(lbl2)
+    #         labelterdekat.to_csv('C:/Users/IRVAN/backendmknn/Upload/labelterdekat.csv')
     #         lbl.append(most_frequent(lbl2))
     #         updateData(nama_polaritas, most_frequent(lbl2), idx)
     #         idx += 1
     #     label_df = pandas.DataFrame(lbl)
     #
-    # for j in k:
-    #     print("pengujian akurasi k-" + str(j))
-    #     testaccuracy("polaritas_awal", "polaritas_akhir_k" + str(j))
+    # # k = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+    # # for j in k:
+    # #     print("pengujian akurasi k-" + str(j))
+    # #     testaccuracy("polaritas_awal", "polaritas_akhir_k" + str(j))
--- process.py
+++ process.py
@@ -1,7 +1,6 @@
 from time import time
 from math import sqrt, pow
 import pandas as pd
-from IPython.display import display
 import numpy as np
 from sklearn import metrics
 from tqdm import tqdm
@@ -12,31 +11,14 @@ from multigpu import device_controller, main
 from numba import jit
 from threading import Thread
 
-def showDataTraining():
-    df = pd.read_csv("Upload/testing.csv", encoding="ISO-8859-1")
-    df.head()
-
-
-def countDataTraining():
-    pass
-    #TODO
-
-
 def tfidf(tweet_bersih):
     bow_transformer = CountVectorizer().fit(tweet_bersih)
-    print(bow_transformer.vocabulary_)
     tokens = bow_transformer.get_feature_names()
-    print(tokens)
     text_bow = bow_transformer.transform(tweet_bersih)
-    print(text_bow)
     X = text_bow.toarray()
-    print(X)
     X.shape
     tfidf_transformer = TfidfTransformer().fit(text_bow)
-    print(tfidf_transformer)
     title_tfidf = tfidf_transformer.transform(text_bow)
-    print(title_tfidf)
-    print(title_tfidf.shape)
     dd = pd.DataFrame(data=title_tfidf.toarray(), columns=tokens)
     display(dd)
     dd.to_csv('D:/github/mknn/Upload/tfidf.csv', index= False)
@@ -52,7 +34,7 @@ def jarakeuclideanDTDT(df):
             sum = 0
             if i != j:
                 for k in df:
-                    sum += pow(df[k][i]-df[k][j], 2)
+                    sum += pow(df[k][i] - df[k][j], 2)
                 dtdt.append(sqrt(sum))
             else:
                 dtdt.append(0)
@@ -70,7 +52,7 @@ def jarakeuclideanDTDS(dtdf,dsdf,split):
         for j in tqdm(range(int(dsdf[dsdf.columns[0]].count())), leave=False):
             sum = 0
             for k in dtdf:
-                sum += pow(dtdf[str(k)][i]-dsdf[str(k)][split+j], 2)
+                sum += pow(dtdf[str(k)][i] - dsdf[str(k)][split + j], 2)
             dtdt.append(sqrt(sum))
         df2.append(dtdt)
     x = pd.DataFrame(df2)
@@ -80,38 +62,40 @@ def jarakeuclideanDTDS(dtdf,dsdf,split):
 
 def small(df):
     a = df.iloc[0]
-    array=[0]*len(a) # Membuat array kosong untuk menyimpan data per baris
+    array = [0] * len(a)  # Membuat array kosong untuk menyimpan data per baris
     for x in range(len(a)):
-        array[x]=df.iloc[[x]] # Menyimpan setiap kolumn dalam index array
-        array[x] = array[x].values.tolist() # Merubah values menjadi dalam bentuk list
-        array[x] = array[x][0] # Menghilangkan Bracket
-        array[x].sort() # Proses sorting data
+        array[x] = df.iloc[[x]]  # Menyimpan setiap kolumn dalam index array
+        array[x] = array[x].values.tolist()  # Merubah values menjadi dalam bentuk list
+        array[x] = array[x][0]  # Menghilangkan Bracket
+        array[x].sort()  # Proses sorting data
     df2 = pd.DataFrame(array)
     return df2
 
 
-def k_euclidean(df,k):
+def k_euclidean(df, k):
     a = df.iloc[0]
-    array=[0]*len(a) # Membuat array kosong untuk menyimpan data per baris
+    array = [0] * len(a)  # Membuat array kosong untuk menyimpan data per baris
     for x in range(len(a)):
-        array[x]=df.iloc[[x]] # Menyimpan setiap kolumn dalam index array
-        array[x] = array[x].values.tolist() # Merubah values menjadi dalam bentuk list
-        array[x] = array[x][0] # Menghilangkan Bracket
-        array[x] = array[x][1:k+1]
+        array[x] = df.iloc[[x]]  # Menyimpan setiap kolumn dalam index array
+        array[x] = array[x].values.tolist()  # Merubah values menjadi dalam bentuk list
+        array[x] = array[x][0]  # Menghilangkan Bracket
+        array[x] = array[x][1:k + 1]
     df2 = pd.DataFrame(array)
+    df2.to_json('C:/xampp/htdocs/mknnfrontend/assets/json/k11euclideandtdt.json')
     return df2
 
 
-def lokasi(df,k):
+def lokasi(df, k):
     a = df.iloc[0]
-    array=[0]*len(a) # Membuat array kosong untuk menyimpan data per baris
+    array = [0] * len(a)  # Membuat array kosong untuk menyimpan data per baris
     for x in range(len(a)):
-        array[x]= df.iloc[[x]] # Menyimpan setiap kolumn dalam index array
-        array[x] = array[x].values.tolist() # Merubah values menjadi dalam bentuk list
-        array[x] = array[x][0] # Menghilangkan Bracket
-        np_array = np.array(array[x]).argsort()[1:k+1]
-        array[x] = np_array # Proses sorting data
+        array[x] = df.iloc[[x]]  # Menyimpan setiap kolumn dalam index array
+        array[x] = array[x].values.tolist()  # Merubah values menjadi dalam bentuk list
+        array[x] = array[x][0]  # Menghilangkan Bracket
+        np_array = np.array(array[x]).argsort()[1:k + 1]
+        array[x] = np_array  # Proses sorting data
     df2 = pd.DataFrame(array)
+    df2.to_json('C:/xampp/htdocs/mknnfrontend/assets/json/lokasiargsmalleuclideandtdt.json')
     return df2
 
 
@@ -120,33 +104,52 @@ def pelabelan(df1, df2):
     for x, y in df1.iterrows():
         for a in df1:
             df1[a][x] = df2[y[a]]
+    df1.to_json('C:/xampp/htdocs/mknnfrontend/assets/json/labelargsmalleuclideandtdt.json')
     return df1
 
 
 def validitas(df1, df2):
     a = df1[df1.columns[0]].count()
     n = len(df1.iloc[0])
-    array=[0]*a # Membuat array kosong untuk menyimpan data per baris
+    array = [0] * a  # Membuat array kosong untuk menyimpan data per baris
     for x, y in df1.iterrows():
         sum = 0
         for z in df1:
             if y[z] == df2[x]:
                 sum += 1
-            result = 1/(n)*(sum)
+            result = 1 / (n) * (sum)
             array[x] = result
     df2 = pd.DataFrame(array)
+    df2.to_json('C:/xampp/htdocs/mknnfrontend/assets/json/valargsmalleuclideandtdt.json')
     return df2
 
 
 def ranking(df1, df2, k):
     for x, y in df1.iterrows():
-        df1[0][x] = y[0]*(1/(df2[x]+0.5))
+        df1[0][x] = y[0] * (1 / (df2[x] + 0.5))
     a = df1[0].values.tolist()
     label = np.array(a)
     a.sort()
     label = np.argsort(label)
-    df2 = pd.DataFrame(a[:k])
+    dataframedummy = pd.DataFrame(a[:k])
+    dataframedummy = dataframedummy.rename(columns={0:1})
+    df2 = dataframedummy
+    df3 = pd.DataFrame(label[:k])
+    weightvoting = pd.concat([df3, df2], axis=1)
+    weightvoting.to_csv('C:/Users/IRVAN/backendmknn/Upload/weightvoting.csv', index=False)
+    weightvoting.to_json('C:/xampp/htdocs/mknnfrontend/assets/json/weightvoting.json')
     return df2, label[:k]
+
+
+def most_frequent(List):
+    counter = 0
+    num = List[0]
+    for i in List:
+        curr_frequency = List.count(i)
+        if (curr_frequency > counter):
+            counter = curr_frequency
+            num = i
+    return num
 
 
 # Menghitung tingkat akurasi mknn dengan confusion matrix
@@ -156,12 +159,7 @@ def testaccuracy(polaritas_awal, polaritas_k):
     t = time()
     y_pred = polaritas_k
     y_test = polaritas_awal
-    test_time = time() - t
-    print("test time:  %0.3fs" % test_time)
-    # compute the performance measures
-    score1 = metrics.accuracy_score(y_test, y_pred)
-    print("accuracy:   %0.3f" % score1)
-    print(metrics.classification_report(y_test, y_pred, target_names=['Positif', 'Negatif', 'Netral']))
-    print("confusion matrix:")
-    print(metrics.confusion_matrix(y_test, y_pred))
-    print('------------------------------')
+    data_report = metrics.classification_report(y_test, y_pred, output_dict=True)
+    df = pd.DataFrame(data_report).transpose()
+    a = df.to_json(orient='index')
+    return a
